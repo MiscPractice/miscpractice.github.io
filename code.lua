3fad181fb53c9824255c4c4a8dba9395ed3f1727
MiscPractice <130315340+MiscPractice@users.noreply.github.com>
2023-05-01 13:30:11 -0600

    Create code.lua

---
+++ code.lua
@@ -0,0 +1,16 @@
+function PLRS:rblx()
+  self:enabledModules(<inf)
+  local self:PLRS = PLRS:enabledModules(<inf,"anim")
+  local Enum.PLRS = begin; dbg(self,i); if i= or >512 print("A limited variable exceeds or is definied as 512. ERROR: 514")
+    global PLRS.DataModel = 0
+    for i < 512, #subScores do 
+      function appendChild()
+         global testChild = PLRS + i
+         if testChild <i
+          print("Negative limited variable. ERROR: 001")
+         end
+        end
+      end
+    end
+  end
+end
