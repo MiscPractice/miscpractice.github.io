c529b39f4dfe6726f3a7096370df762f0b75847c
MiscPractice <130315340+MiscPractice@users.noreply.github.com>
2023-05-02 12:58:26 -0600

    Update code.lua

--- code.lua
+++ code.lua
@@ -1,16 +1,24 @@
+local ping = -1+PLRS
 function PLRS:rblx()
   self:enabledModules(<inf)
   local self:PLRS = PLRS:enabledModules(<inf,"anim")
   local Enum.PLRS = begin; dbg(self,i); if i= or >512 print("A limited variable exceeds or is defined as being at 512. ERROR: 514")
-    global PLRS.DataModel = 0
+    global PLRS:DataModel = 0
     for i < 512, #subScores do 
       function appendChild()
+         global n = -1
          global testChild = PLRS + i
-         if testChild <i
+         if testChild <i or i<n
           print("Negative limited variable. ERROR: 001")
          end
         end
       end
     end
   end
+end
+PLRS:appendChild(testChild);
+PLRS:WaitForChild(testChild);
+PLRS:ping(n);
+if ping > -1
+ print("YOU CAN SEE THE FUTURE! Yes, really.")
 end
