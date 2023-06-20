f5b0427a31d7f88f4e40ddb2cd8813293084acce
MiscPractice <130315340+MiscPractice@users.noreply.github.com>
2023-06-19 22:25:28 -0600

    Create circle.py

---
+++ code_by_ai/circle.py
@@ -0,0 +1,14 @@
+import turtle
+
+# create a turtle object
+t = turtle.Turtle()
+
+# set the pen size and color
+t.pensize(3)
+t.pencolor("black")
+
+# draw a circle
+t.circle(100)
+
+# wait for the user to close the window
+turtle.done()
